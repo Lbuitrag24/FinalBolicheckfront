d31a161f1008c2ed191c05267bd821b7051c2738
lbuitrag24 <lebuitrago24@soy.sena.edu.co>
2025-03-31 16:24:34 -0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Esto permite que React Router maneje las rutas
-  },
-  base: '/',  // Ajusta esto si tu aplicación no está en la raíz del dominio
+  base: '/', // Esto es clave para evitar problemas con las rutas en producción.
 });

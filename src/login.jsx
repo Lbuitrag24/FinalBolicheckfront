4677d9a0c60a528c725facd1a8f001263a07e7ce
lbuitrag24 <lebuitrago24@soy.sena.edu.co>
2025-03-31 18:10:56 -0500

    CUALQUIER

--- src/login.jsx
+++ src/login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8080/api/login", {
+      const response = await fetch("https://bolicheck.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
